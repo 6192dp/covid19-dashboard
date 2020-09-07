e2be6f2bcdcbfc90b424b7ef186cb9fe80099c81
6192dp <prasad.mvss1@gmail.com>
2020-09-07 16:49:38 +0530

    changing favicon

--- src/shells/Login/header.jsx
+++ src/shells/Login/header.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./styles.css";
+//import socialicon from "../../icons/social.svg";
 
 export const Header = props => {
-  return <div className="root_hdr">{props.headerTitle}</div>;
+  return (
+    <div className="root_hdr">
+      {props.headerTitle}
+      {/* <img src={socialicon} className="img_social" /> */}
+    </div>
+  );
 };
